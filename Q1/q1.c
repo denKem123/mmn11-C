24d363479d5fbe6c39f02ba8718f2411b417f746
Guy Michalovitz <guym@landa-corp.com>
2023-03-19 19:03:47 +0200

    commit

---
+++ Q1/q1.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#define MAXLINE 1000
+#define MINLINE 80
+
+int my_strcmp(char cs[], char ct[]);
+void my_strncmp(char cs[], char ct[]);
+void copy(char to[], int from);
+
+int main()
+{
+    printf("\nHello World\n");
+    /*int len;
+    char line[MAXLINE];
+
+    while ((len = getline(line, MAXLINE)) > 0)
+        if (len > MINLINE)
+            printf("%s", line);
+            */
+    return 0;
+}
+
+/* getline: read a line into s, return length */
+int my_strcmp(char cs[], char ct[])
+{
+    int i;
+    for (i = 0; cs[i] || ct[i]; i++)
+    {
+        if (cs[i] != ct[i])
+            return cs[i] - ct[i];
+    }
+    return cs[i] - ct[i];
+}
+
+void removeblank(char line[], int linelength)
+{
+    int i = 0;
+
+    while (line[i] != '\0' && i < linelength - 1)
+    {
+        if (line[i] == '\t' || line[i] == ' ')
+            line[i] = line[i + 1];
+        else
+            i++;
+    }
+    if (line[i] == '\t' || line[i] == ' ')
+        line[i] = '\0';
+}
