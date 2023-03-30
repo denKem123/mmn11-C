609e4645460a7df0062143a8efbda3ac5b1c591f
Guy Michalovitz <guym@landa-corp.com>
2023-03-30 19:16:47 +0300

    commit

--- Q2/q2.c
+++ Q2/q2.c
@@ -1,51 +1,35 @@
 #include <stdio.h>
-#define MAXLINE 1000
-#define MINLINE 80
 
-int getline(char line[], int maxline);
-void removeblank(char line[],int linelength);
-void copy(char to[], int from);
+int bits_count(unsigned long l);
 
 int main()
 {
-    printf("\nHello World\n");
-    /*int len;
-    char line[MAXLINE];
-
-    while ((len = getline(line, MAXLINE)) > 0)
-        if (len > MINLINE)
-            printf("%s", line);
-            */
-    return 0;
-    
+    int w;
+    unsigned long n;
+    char msg1[] = "\nEnter a positive long int - to see how may of his even located bits are on: \n";
+    do
+    {
+        printf("%s",msg1);
+        w = scanf("%lu", &n);        
+        if (w == EOF)
+            return -1;
+    } while (w != 2);
+    printf("\nthe number you entered is: %lu", n);
+    int count = its_count(n);
+    printf("\nhis number of even bits that are turned on is: %d", count);
 }
 
-/* getline: read a line into s, return length */
-int getline(char line[], int maxline)
+/* 
+    its_count: return the count of all the on bits that are in an even location.
+*/
+int its_count(unsigned long l)
 {
-    int i, c;
+    int count = 0;
+    unsigned long mask;
 
-    for (i = 0; i < maxline - 1 && (c = getchar()) != EOF; i++)
+    for (mask = 1 << (sizeof(unsigned long) * 8 - 2); mask; mask >>= 2)
     {
-        line[i] = c;
-        if (c != '\n')
-            break;
+        count += l & mask ? 1 : 0;
     }
-    line[i] = '\0';
-    return i;
-}
-
-void removeblank(char line[], int linelength)
-{
-    int i = 0;
-
-    while (line[i] != '\0' && i < linelength - 1)
-    {
-        if (line[i] == '\t' || line[i] == ' ')
-            line[i] = line[i + 1];
-        else
-            i++;
-    }
-    if (line[i] == '\t' || line[i] == ' ')
-        line[i] = '\0';
+    return count;
 }
